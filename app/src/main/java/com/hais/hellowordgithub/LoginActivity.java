9e8d07ced1a5caafb294238c1d29b49a50ba4884
haishuang <623691640@qq.com>
2016-08-04 17:44:02 +0800

    test-2

--- app/src/main/java/com/hais/hellowordgithub/LoginActivity.java
+++ app/src/main/java/com/hais/hellowordgithub/LoginActivity.java
@@ -2,37 +2,23 @@ package com.hais.hellowordgithub;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
-
-import com.google.android.gms.common.SignInButton;
+import android.util.Log;
 
 
 /**
  * A login screen that offers login via email/password and via Google+ sign in.
- * <p/>
+ * <p>
  * ************ IMPORTANT SETUP NOTES: ************
  * In order for Google+ sign in to work with your app, you must first go to:
  * https://developers.google.com/+/mobile/android/getting-started#step_1_enable_the_google_api
  * and follow the steps in "Step 1" to create an OAuth 2.0 client for your package.
  */
 public class LoginActivity extends Activity {
-
-    // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mEmailLoginFormView;
-    private SignInButton mPlusSignInButton;
-    private View mSignOutButtons;
-    private View mLoginFormView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        Log.e("yyy","fcghjkl;");
     }
 }
 

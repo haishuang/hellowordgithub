4345607f089b6447fabb7c4b242d39ad9bae491c
haishuang <623691640@qq.com>
2016-09-02 16:59:30 +0800

    test 201609021608

---
+++ app/src/main/java/com/hais/hellowordgithub/TEs.java
@@ -0,0 +1,13 @@
+package com.hais.hellowordgithub;
+
+import android.util.Log;
+
+/**
+ * Created by Huang hai-sen on 2016/9/2.
+ */
+public class TEs {
+    int p;
+    private void s(){
+
+    }
+}
